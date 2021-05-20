2740379aa1c245370866498395d5e4909ffb3b02
krava9999 <kosmo@internet.ru>
2021-05-20 16:43:51 +1000

    Блоки до Imax без слайдера

---
+++ js/ImaxPlayer.js
@@ -0,0 +1,6 @@
+﻿let imaxVideo = document.querySelector(".Imax__video");
+let imaxPlayBtn = document.querySelector(".Imax__play-btn");
+imaxPlayBtn.addEventListener("click", function () {
+    imaxPlayBtn.style.display = "none";
+    imaxVideo.setAttribute("controls", true);
+})
---
+++ js/menu.js
@@ -0,0 +1,13 @@
+﻿let burgerBtn = document.querySelector(".header__burger");
+let closeBtn = document.querySelector(".header__close-btn");
+let hideMenu = document.querySelector(".header__hide-menu");
+burgerBtn.addEventListener("click", function () {
+    burgerBtn.style.display = "none";
+    closeBtn.style.display = "block";
+    hideMenu.style.display = "flex";
+})
+closeBtn.addEventListener("click", function () {
+    burgerBtn.style.display = "block";
+    closeBtn.style.display = "none";
+    hideMenu.style.display = "none";
+})
